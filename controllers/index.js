6cde5ac223a156944d8d7b2060cfc5e80dde0617
Jandy Kiger <76858411+jandyrae@users.noreply.github.com>
2022-09-16 16:23:35 -0700

    if else status add

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,11 @@
 const data = "Jason Kiger at controllers index";
 
 const index = (_req, res) => {
-  res.status(200).send(data);
-  res.status(503).send("Voldemort says server error!")
+  if (res.status(200)) {
+    res.send(data);
+  } else {
+    res.send("Voldemort says server error!");
+  }
 };
 
 module.exports = {
