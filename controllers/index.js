f68f6a846145d8947fb730ad5d09bcf9223f6cc6
Jandy Kiger <76858411+jandyrae@users.noreply.github.com>
2022-09-16 16:18:12 -0700

    added status

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,8 @@
 const data = "Jason Kiger at controllers index";
 
 const index = (_req, res) => {
-  res.send(data);
+  res.status(200).send(data);
+  res.status(503).send("Voldemort says server error!")
 };
 
 module.exports = {
