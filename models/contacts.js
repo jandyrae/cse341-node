c24d1b555c529896ab6a30ee3af0716e197e478c
Jandy Kiger <76858411+jandyrae@users.noreply.github.com>
2022-09-30 17:25:45 -0700

    commented out functions in models folder need to learn how to integrate them

--- models/contacts.js
+++ models/contacts.js
@@ -1,3 +1,30 @@
-await db.collection('contacts').insertMany([
 
-])
+// const { ObjectId} = require("mongodb");
+// const {newEntry} = require('../controllers/contacts')
+
+// // create new record in database
+
+// const createContact = db.collection('contacts')
+// .insertOne(newEntry, function(err, res) {
+//     if (err) throw err;
+//     console.log(`${res.insertedCount} document inserted`);
+//     console.log(res.insertedIds);
+//     db.close();
+//   });
+
+// // update existing record in database
+// const updateContact = db.collection('contacts').updateOne([
+//     // updateOne(filter, update, options, callback)
+//     { _id: ObjectId(req.params.id) },
+//     {firstName: ''},
+//     {lastName: ''},
+//     {email: ''},
+//     {birthday: ''}
+// ])
+
+// // delete records from database
+// const deleteContact = db.collection('contacts').deleteOne([
+//     { _id: ObjectId(req.params.id) }
+// ])
+
+// module.exports = {createContact, updateContact, deleteContact}
