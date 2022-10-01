061be50cb988ccc1dfe4c26eb6c6236e29e0e5ad
Jandy Kiger <76858411+jandyrae@users.noreply.github.com>
2022-09-30 17:22:43 -0700

    adjusted root index file to correct route path

--- index.js
+++ index.js
@@ -7,16 +7,17 @@ const port = config.PORT || 8080;
 const connectDB = require('./models/connectDB')
 
 app
+  // .use(bodyParser.urlencoded({ extended: false }))
   .use(bodyParser.json())
   .use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     console.log('Time: ', Date.now())
     next();
   })
-  .use('/', require('./routes'));
+  .use('/', require('./routes/index'));
 
 app.listen(port, () => {
-  console.log(`Application listening on http://localhost:8080/contacts  or ${port}`);
+  console.log(`Application listening on http://localhost:8080/ or ${port}`);
 });
 
 connectDB().catch(console.error);
