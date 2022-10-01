5d5e76ab854b6730584fc356a28c57a026a11833
Jandy Kiger <76858411+jandyrae@users.noreply.github.com>
2022-09-30 17:24:47 -0700

    used save env variable for dbname

--- models/connectDB.js
+++ models/connectDB.js
@@ -1,11 +1,11 @@
 const { MongoClient, ServerApiVersion } = require("mongodb");
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
 
-// For memoizing the db
+// For memoizing the db for optimization
 let db;
 
-const initialize = async () => {
+const getDB = async () => {
   if (db) return db;
   try {
     const client = new MongoClient(process.env.DB_URI, {
@@ -15,8 +15,8 @@ const initialize = async () => {
     });
 
     await client.connect();
-    console.log("Connected successfully to DB");
-    db = client.db('CSE341');
+    console.log(`Connected successfully to DB ${process.env.DB_NAME}`);
+    db = client.db(process.env.DB_NAME);
 
     return db;
   } catch (error) {
@@ -24,4 +24,4 @@ const initialize = async () => {
   }
 };
 
-module.exports = initialize;
+module.exports = getDB;
